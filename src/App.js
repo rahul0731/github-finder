dfbc3e5faf79b55d7b24614a7f48d8f061db06e1
rahul07 <rahul.yadav@digitalshark.in>
2020-10-04 08:15:50 +0530

    Adding Serach functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 class App extends Component {
   state = {
     users : [],
     loading : false
   }
   async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
     this.setState({ loading : true});
 
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
@@ -17,12 +17,21 @@ class App extends Component {
     this.setState({users : res.data ,loading : false})
      
   }
+  //Search Github users
+  searchUsers = async text =>{
+    this.setState({loading :true});
+    const res = await axios
+    .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+   console.log( res.data.items);
+    this.setState({users : res.data.items ,loading : false})
+  }
   render(){
     return(
       <div className='App'>
         <Navbar  title ="Github Finder"
         icon ='fab fa-github'/>
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
         <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
